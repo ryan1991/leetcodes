e86b829a7e8f726a0a2ab635938a207a810aa986
ryan1991 <1017242643@qq.com>
2019-04-02 19:33:03 +0800

    三数之和

--- src/main/java/ThreeSum.java
+++ src/main/java/ThreeSum.java
@@ -15,7 +15,7 @@ public class ThreeSum {
 
 
     public  List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> rst = new ArrayList<>();
+        Set<List<Integer>> rst = new HashSet<>();
         Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
             int n = nums[i];
@@ -47,7 +47,7 @@ public class ThreeSum {
         }
 
 
-        return rst;
+        return new ArrayList<>(rst);
     }
 
 }
