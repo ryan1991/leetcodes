7c6bfa99ef18c6b20e8e52ff4f4ab62ce3e16ef1
ryan1991 <1017242643@qq.com>
2019-09-09 17:00:38 +0800

    合并k排序

---
+++ src/main/java/datastruct/MergeKOrderedList.java
@@ -0,0 +1,102 @@
+package datastruct;
+
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * 合并K个排序链表
+ */
+public class MergeKOrderedList {
+
+
+    public static void main(String[] args) {
+
+        ListNode[] list  = new ListNode[3];
+
+        ListNode head1 = new ListNode(1);
+        head1.next = new ListNode(4);
+        head1.next.next = new ListNode(5);
+
+
+        ListNode head2 = new ListNode(1);
+        head2.next = new ListNode(3);
+        head2.next.next = new ListNode(4);
+
+        ListNode head3 = new ListNode(2);
+        head3.next = new ListNode(6);
+
+
+        list[0] = head1;
+
+        list[1] = head2;
+
+        list[2] = head3;
+
+
+        ListNode dummy =  mergeKLists(list);
+
+
+
+    }
+
+
+
+
+
+    public static ListNode mergeKLists(ListNode[] lists) {
+        int len = lists.length;
+
+        if (len == 0)
+            return null;
+
+        if (len == 1)
+            return lists[0];
+
+
+        List<Integer> nodes = new ArrayList<>();
+
+
+        for (ListNode node : lists){
+            while (node != null) {
+                nodes.add(node.val);
+                node = node.next;
+            }
+
+        }
+
+        nodes.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return Integer.compare(o1, o2);
+            }
+        });
+
+        ListNode head = new ListNode(0);
+        ListNode other = head;
+
+        for (Integer val : nodes){
+            other.next = new ListNode(val);
+            other = other.next;
+
+        }
+
+        return head.next;
+
+    }
+
+
+
+    private static class ListNode{
+        int val;
+
+        ListNode next;
+
+        ListNode(int val){
+            this.val = val;
+
+        }
+
+    }
+}
---
+++ src/main/java/datastruct/MergeTwoOrderedList.java
@@ -0,0 +1,61 @@
+package datastruct;
+
+/**
+ * 合并2个有序链表
+ */
+public class MergeTwoOrderedList {
+
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
+
+
+    public ListNode mergeTwoLists(ListNode l1,  ListNode l2){
+
+        if (l1 == null)
+            return l2;
+
+        if (l2 == null)
+            return l1;
+
+
+        if (l1.val < l2.val){
+            l1.next = mergeTwoLists(l1.next, l2);
+
+            return l1;
+        }else {
+
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+    private class ListNode{
+        int val;
+
+        ListNode next;
+
+        ListNode(int val){
+            this.val = val;
+
+        }
+
+    }
+
+
+}
